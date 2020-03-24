2373a95c917ecb6f27604391e24bd487a3be0a98
nitunwa <nitun.2@gmail.com>
2020-03-24 11:59:19 -0700

    add slide-up section

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
-// $(document).ready(function(){
-
-//   $("#slide-down").click(function(){
-//     $("#slide.up").slideDown("slow");
-    
-//   });
-
-
-// });
-
 $(document).ready(function(){
   $("#flip").click(function(){
     $("#panel").slideDown("slow");
   });
+ 
+  $("#slide").click(function(){
+    $("#slide-up").slideUp("slow");
+  });
+
 });
